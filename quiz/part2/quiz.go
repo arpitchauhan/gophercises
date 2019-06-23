8a0e775f480edc9c9d320de84c2a8802e82ca3aa
Arpit Chauhan <arpitchauhan@gmail.com>
2019-06-24 01:51:12 +0530

    Add exercise 1 part 2, 30 second time limit

---
+++ quiz/part2/quiz.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getReaderFromFile(filename string) (io.ReadCloser, error) {
+	reader, err := os.Open(filename)
+	return reader, err
+}
+
+func extractRecordsFromReader(reader io.ReadCloser) ([][]string, error) {
+	defer reader.Close()
+	return csv.NewReader(reader).ReadAll()
+}
+
+func askQuestions(scanner *bufio.Scanner, questionsWithAnswers [][]string, score *int, done chan<- bool) {
+	for index, questionsWithAnswer := range questionsWithAnswers {
+		question, correctAnswer := questionsWithAnswer[0], questionsWithAnswer[1]
+
+		fmt.Printf("Problem #%v: %v = ", index+1, question)
+		scanner.Scan()
+
+		userAnswer := scanner.Text()
+
+		if userAnswer == correctAnswer {
+			*score++
+		}
+	}
+
+	done <- true
+}
+
+func conductQuiz(scanner *bufio.Scanner, questionsWithAnswers [][]string, timeLimit time.Duration) int {
+	score := 0
+
+	quizDone := make(chan bool)
+
+	timer := time.NewTimer(timeLimit)
+
+	go askQuestions(scanner, questionsWithAnswers, &score, quizDone)
+
+	select {
+	case <-quizDone:
+		return score
+	case <-timer.C:
+		return score
+	}
+}
+
+func main() {
+	reader, err := getReaderFromFile("../problems.csv")
+	check(err)
+
+	records, err := extractRecordsFromReader(reader)
+	check(err)
+
+	userScore := conductQuiz(bufio.NewScanner(os.Stdin), records, 30*time.Second)
+
+	fmt.Printf("\nYou scored %v out of %v.", userScore, len(records))
+}
