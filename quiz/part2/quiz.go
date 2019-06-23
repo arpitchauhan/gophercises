27bfe92a25f831d91186da97d7361b1ed7fd9af3
Arpit Chauhan <arpitchauhan@gmail.com>
2019-06-24 02:25:18 +0530

    Allow shuffling order of questions in quiz

--- quiz/part2/quiz.go
+++ quiz/part2/quiz.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
 	"time"
 )
@@ -26,7 +27,13 @@ func extractRecordsFromReader(reader io.ReadCloser) ([][]string, error) {
 	return csv.NewReader(reader).ReadAll()
 }
 
-func askQuestions(scanner *bufio.Scanner, questionsWithAnswers [][]string, score *int, done chan<- bool) {
+func askQuestions(scanner *bufio.Scanner, questionsWithAnswers [][]string, shuffle bool, score *int, done chan<- bool) {
+	if shuffle {
+		rand.Shuffle(len(questionsWithAnswers), func(i, j int) {
+			questionsWithAnswers[i], questionsWithAnswers[j] = questionsWithAnswers[j], questionsWithAnswers[i]
+		})
+	}
+
 	for index, questionsWithAnswer := range questionsWithAnswers {
 		question, correctAnswer := questionsWithAnswer[0], questionsWithAnswer[1]
 
@@ -43,14 +50,14 @@ func askQuestions(scanner *bufio.Scanner, questionsWithAnswers [][]string, score
 	done <- true
 }
 
-func conductQuiz(scanner *bufio.Scanner, questionsWithAnswers [][]string, timeLimit time.Duration) int {
+func conductQuiz(scanner *bufio.Scanner, questionsWithAnswers [][]string, shuffle bool, timeLimit time.Duration) int {
 	score := 0
 
 	quizDone := make(chan bool)
 
 	timer := time.NewTimer(timeLimit)
 
-	go askQuestions(scanner, questionsWithAnswers, &score, quizDone)
+	go askQuestions(scanner, questionsWithAnswers, shuffle, &score, quizDone)
 
 	select {
 	case <-quizDone:
@@ -61,8 +68,11 @@ func conductQuiz(scanner *bufio.Scanner, questionsWithAnswers [][]string, timeLi
 }
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
 	timeLimitPtr := flag.Int("limit", 30, "the time limit for quiz in seconds")
 	csvFilePathPtr := flag.String("csv", "../problems.csv", "the path of the CSV file")
+	shufflePtr := flag.Bool("shuffle", false, "Whether to shuffle the order of questions")
 
 	flag.Parse()
 
@@ -72,7 +82,7 @@ func main() {
 	records, err := extractRecordsFromReader(reader)
 	check(err)
 
-	userScore := conductQuiz(bufio.NewScanner(os.Stdin), records, time.Duration(*timeLimitPtr)*(time.Second))
+	userScore := conductQuiz(bufio.NewScanner(os.Stdin), records, *shufflePtr, time.Duration(*timeLimitPtr)*(time.Second))
 
 	fmt.Printf("\nYou scored %v out of %v.", userScore, len(records))
 }
